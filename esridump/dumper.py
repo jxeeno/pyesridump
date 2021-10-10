1bae4f095c6502f4144abd2997d7b5db76cf9dc9
Kenneth Tsang <me@jxeeno.com>
2021-10-10 15:42:21 +1100

    feat(): use startWith for oid

--- esridump/dumper.py
+++ esridump/dumper.py
@@ -350,6 +350,9 @@ class EsriDumper(object):
                 # to help build the pages
                 try:
                     (oid_min, oid_max) = self._get_layer_min_max(oid_field_name)
+                    
+                    if self._startWith:
+                        oid_min = self._startWith - 1
 
                     for page_min in range(oid_min - 1, oid_max, page_size):
                         page_max = min(page_min + page_size, oid_max)
